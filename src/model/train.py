6123f023b112f4bff45d9992eddd4e86382bcace
Jordan Jenkins <jenkins.587@gmail.com>
2023-08-27 09:13:39 -0400

    format train.py

--- src/model/train.py
+++ src/model/train.py
@@ -36,8 +36,10 @@ def get_csvs_df(path):
 
 
 def split_data(df):
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness',
-               'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
+            'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree',
+            'Age']].values
+    y = df['Diabetic'].values
     X_train, X_test, y_train, y_test = train_test_split(X,
                                                         y,
                                                         test_size=0.30,
